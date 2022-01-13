7398e8c30d856260023b3361a09c36006d9ab7a4
Kesyza <kesyza_210@smkassalaambandung.sch.id>
2022-01-13 11:43:04 +0700

    1

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,14 +7,65 @@ use App\Models\Sopir;
 
 class ApiController extends Controller
 {
-    public function dataSopir(){
+    public function index(){
+        //menampilkan data sopir pada JSON
         $sopir = Sopir::all();
-        
+
         //ubah ke JSON
         return response()->json([
             'success' => true,
             'message' => 'List Data Sopir',
             'data'    => $sopir  
         ], 200);
+    }
+
+    public function create(){
+        //
+    }
+
+    public function store(Request $request){
+        //menambah data sopir pada JSON
+        $request->validate([
+            'nama_sopir' => 'required',
+            'alamat' => 'required',
+            'nomor_hp' => 'required',
+        ]);
+
+        $sopir = new Sopir;
+        $sopir->nama_sopir = $request->nama_sopir;
+        $sopir->alamat = $request->alamat;
+        $sopir->nomor_hp = $request->nomor_hp;
+        $sopir->save();
+
+        //ubah ke JSON
+        return response()->json([
+            'success' => true,
+            'message' => 'List Tambah Sopir',
+            'data'    => $sopir  
+        ], 200);
+    }
+
+    public function show($id){
+        //menampilkan data sopir pada JSON
+        $sopir = Sopir::findOrFail($id);
+
+        //ubah ke JSON
+        return response()->json([
+            'success' => true,
+            'message' => 'List Data Sopir',
+            'data'    => $sopir  
+        ], 200);
+    }
+
+    public function edit(){
+        //
+    }
+
+    public function update(){
+        //
+    }
+
+    public function destroy(){
+        //
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('sopir2',[ApiController::class, 'dataSopir']);
+Route::resource('sopir2',ApiController::class);
